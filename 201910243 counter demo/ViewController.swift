acee73bae674cb2129d80a5074bc5874129499e0
hyu644 <67137241+hyu644@users.noreply.github.com>
2020-07-13 20:14:25 +0900

    firstcomment

--- 201910243 counter demo/ViewController.swift
+++ 201910243 counter demo/ViewController.swift
@@ -10,11 +10,60 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var display: UILabel!
+    @IBOutlet weak var addbutton: UIButton!
+    @IBOutlet weak var subtractButton: UIButton!
+    @IBOutlet weak var clearButton: UIButton!
+    @IBOutlet weak var backButton: UIButton!
+    
+    
+    var count = 0
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        backButton.isHidden = true
     }
 
-
+    func updateDisplay(){
+        display.text = String(format:"%04d",count)
+    }
+    
+    @IBAction func addAction(_ sender: UIButton) {
+        count = count + 1
+        if(count > 9999){
+            count = 0
+        }
+        updateDisplay()
+    }
+    @IBAction func subtractAction(_ sender: Any) {
+        count = count - 1
+        if(count < 0){
+            count = 0
+        }
+        updateDisplay()
+    }
+    
+    @IBAction func clearAction(_ sender: UIButton) {
+        count = 0
+        updateDisplay()
+    }
+    
+    @IBAction func trap(_ sender: UIButton) {
+        display.isHidden = true
+        addbutton.isHidden = true
+        subtractButton.isHidden = true
+        clearButton.isHidden = true
+        backButton.isHidden = false
+        
+    }
+        
+    @IBAction func back(_ sender: UIButton) {
+        display.isHidden = false
+        addbutton.isHidden = false
+        subtractButton.isHidden = false
+        clearButton.isHidden = false
+        backButton.isHidden = true
+    }
 }
 
---
+++ 20191107 keylock not complete/keylock/keylock/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  keylock
+//
+//  Created by hyu on 11/7/1 R.
+//  Copyright © 1 Reiwa hyu. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
